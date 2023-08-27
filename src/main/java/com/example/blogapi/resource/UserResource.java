f8152cd795272938e41884b498c7794ac1ccfacf
Breno <breno.am2004@gmail.com>
2023-08-26 23:38:06 -0300

    feat: exception handler controller method implemented and findById method too

--- src/main/java/com/example/blogapi/resource/UserResource.java
+++ src/main/java/com/example/blogapi/resource/UserResource.java
@@ -5,6 +5,7 @@ import com.example.blogapi.dto.UserDTO;
 import com.example.blogapi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,13 @@ public class UserResource {
         List<UserDTO> listDTO = list.stream().map(item -> new UserDTO(item)).toList();
 
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findAll(@PathVariable String id) {
+        UserDTO userDTO = new UserDTO(userService.findById(id));
+
+        return ResponseEntity.ok().body(userDTO);
     }
 
 }
--- src/main/java/com/example/blogapi/services/UserService.java
+++ src/main/java/com/example/blogapi/services/UserService.java
@@ -2,10 +2,12 @@ package com.example.blogapi.services;
 
 import com.example.blogapi.domain.User;
 import com.example.blogapi.repository.UserRepository;
+import com.example.blogapi.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 // Para ser injetável
 @Service
@@ -17,5 +19,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        // Retorna null ou o obj no repository.findById(). Se não for null, return obj. Se for, orElseThrow com a exception
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
