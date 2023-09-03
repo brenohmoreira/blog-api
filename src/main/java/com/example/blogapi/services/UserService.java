ca865b95e91a4bd5202b54cf884cf989bba31f72
Breno <breno.am2004@gmail.com>
2023-09-02 22:43:42 -0300

    delete method service implemented

--- src/main/java/com/example/blogapi/services/UserService.java
+++ src/main/java/com/example/blogapi/services/UserService.java
@@ -32,6 +32,12 @@ public class UserService {
         return repository.insert(user);
     }
 
+    public void delete(String id) {
+        // Reaproveitar o tratamento de exceção de findById
+        findById(id);
+        repository.deleteById(id);
+    }
+
     public User fromDTO(UserDTO userDTO) {
         return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
