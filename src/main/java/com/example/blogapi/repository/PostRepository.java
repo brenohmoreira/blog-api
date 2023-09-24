6729148cc02a5cbe4068d2863731f7415bb7e53d
Breno <breno.am2004@gmail.com>
2023-09-23 23:21:09 -0300

    post repository implemented

---
+++ src/main/java/com/example/blogapi/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.example.blogapi.repository;
+
+import com.example.blogapi.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
